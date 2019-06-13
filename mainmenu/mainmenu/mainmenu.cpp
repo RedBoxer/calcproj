6dbc3232c2127ae15c0d843593b2e7eb3f1aaf68
RedBoxer <52marba@mail.ru>
2019-06-13 11:20:24 +0300

    Fixed bugs
    Fixed bug in div displaying
    Fixed bug in menu

---
+++ mainmenu/mainmenu/mainmenu.cpp
@@ -0,0 +1,38 @@
+// mainmenu.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	char choice='1';
+	int num=0;
+	int num2=0;
+	while (true){
+		system("cls");
+		printf("Choose operation for numbers(+,/) or 'e' for exit\n>");
+		fflush(stdin);
+		gets(&choice);
+		if (choice == 'e'){
+			exit(0);
+		}
+		printf("Enter first number:\n>");
+		scanf("%d", &num);
+		printf("Enter second number:\n>");
+		scanf("%d", &num2);
+		if ((num != 0) && (num2 != 0)){
+			if (choice == '+'){
+				Sum(num, num2);
+			}
+			if (choice == '/'){
+				Div(num, num2);
+			}
+		}
+		else{
+			printf("Input all numbers and try again\n");
+		}
+		getch();
+	}
+}
+
